9d0eeeb4bed18d292abc638edd66b661aa9bf8b3
QuicknetDevs <canalesdigitales@quicknet.com>
2025-07-27 21:49:21 -0600

    Migrando proyecto desde cuenta jesxus12

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/whatsapp-saliente-wxcc/', // 👈 mismo nombre que el repo
   plugins: [react()],
   build: {
     sourcemap: true,
